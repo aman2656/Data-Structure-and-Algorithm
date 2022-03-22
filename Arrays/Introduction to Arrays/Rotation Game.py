a2065022eb5e1032d7c6e2f4d4c39e4653f5289d
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:11:22 +0530

    Update Rotation Game.py

--- Arrays/Introduction to Arrays/Rotation Game.py
+++ Arrays/Introduction to Arrays/Rotation Game.py
@@ -1,1 +1,32 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    n = [int(i) for i in input().split()]
+    nums = n[1:]
+    b = int(input())
+    b = b % n[0]
+    l = 0
+    u = n[0]-b-1
+    while l<u:
+        nums[l], nums[u] = nums[u], nums[l]
+        l+=1
+        u-=1
+    l = n[0]-b
+    u = n[0]-1
+    while l<u:
+        nums[l], nums[u] = nums[u], nums[l]
+        l+=1
+        u-=1
+    l=0
+    u=n[0]-1
+    while l<u:
+        nums[l], nums[u] = nums[u], nums[l]
+        l+=1
+        u-=1
+    for j in nums:
+        print(j, end = " ")
+    return 0
 
+if __name__ == '__main__':
+    main()
