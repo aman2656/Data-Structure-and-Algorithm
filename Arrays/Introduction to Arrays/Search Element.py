3683786912658ce7d9e6898cc849a2b8b21fa7f8
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:24:23 +0530

    Update Search Element.py

--- Arrays/Introduction to Arrays/Search Element.py
+++ Arrays/Introduction to Arrays/Search Element.py
@@ -1,1 +1,22 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    t = int(input())
+    for i in range(t):
+        a = [int(x) for x in input().split()]
+        b = int(input())
+        # a = a[0]
+        nums = a[1:]
+        f=0
+        for j in nums:
+            if j==b:
+                print('1')
+                f = 1
+                break
+        if f == 0:
+            print('0')
+    return 0
 
+if __name__ == '__main__':
+    main()
