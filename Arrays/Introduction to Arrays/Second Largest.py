c01394da72d9e6b734f67a27fe66821c0ec4a299
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:37:20 +0530

    Update Second Largest.py

--- Arrays/Introduction to Arrays/Second Largest.py
+++ Arrays/Introduction to Arrays/Second Largest.py
@@ -1,3 +1,6 @@
+"""
+You are given an integer array A. You have to find the second largest element/value in the array or report that no such element exists.
+"""
 def second_largest(arr,n):
     ans = None
     # YOUR CODE GOES HERE
