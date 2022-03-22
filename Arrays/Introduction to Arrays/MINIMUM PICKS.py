b2000bf69678a632671d327b55e2e8dce5314842
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:26:50 +0530

    Update MINIMUM PICKS.py

--- Arrays/Introduction to Arrays/MINIMUM PICKS.py
+++ Arrays/Introduction to Arrays/MINIMUM PICKS.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        e = [i for i in A if i%2==0]
+        o = [i for i in A if i%2!=0]
+        return max(e)-min(o)
