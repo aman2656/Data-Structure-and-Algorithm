8298f2d93eefe2832d7cc3ac94d7c90f8528ef6a
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:38:16 +0530

    Update Separate Odd Even.py

--- Arrays/Introduction to Arrays/Separate Odd Even.py
+++ Arrays/Introduction to Arrays/Separate Odd Even.py
@@ -1,3 +1,10 @@
+"""
+You are given an integer T denoting the number of test cases. For each test case, you are given an integer array A.
+
+You have to put the odd and even elements of array A in 2 separate arrays and print the new arrays.
+
+NOTE: Array elements should have the same relative order as in A.
+"""
 def main():
     # YOUR CODE GOES HERE
     # Please take input and print output to standard input/output (stdin/stdout)
