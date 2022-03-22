bab71661c53c33457429dee40b17b01dbbb5379b
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:25:16 +0530

    Update Little Ponny and Maximum Element.py

--- Arrays/Introduction to Arrays/Little Ponny and Maximum Element.py
+++ Arrays/Introduction to Arrays/Little Ponny and Maximum Element.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        c = 0
+        for i in A:
+            if B<i:
+                c+=1
+        if B in A:
+            return c
+        else:
+            return -1
