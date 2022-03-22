439d04fe76468611bd57f68a118a48bc8dd2f49e
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 20:18:10 +0530

    Update MINIMUM PICKS.py

--- Arrays/Introduction to Arrays/MINIMUM PICKS.py
+++ Arrays/Introduction to Arrays/MINIMUM PICKS.py
@@ -6,14 +6,6 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     def solve(self, A):
-        if len(A)==2:
-            if A[0]%2==0:
-                max_even = A[0]
-                min_odd = A[1]
-            else:
-                max_even = A[1]
-                min_odd = A[0]
-            return max_even - min_odd
         max_even = -1000000
         min_odd = 1000000
         for i in A:
