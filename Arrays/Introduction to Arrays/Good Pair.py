b6721fe961892ee53c9a424d476f1bbfa1fae06e
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:22:41 +0530

    Update Good Pair.py

--- Arrays/Introduction to Arrays/Good Pair.py
+++ Arrays/Introduction to Arrays/Good Pair.py
@@ -1,1 +1,12 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        for i in range(0, len(A)-1):
+            for j in range(i+1, len(A)):
+                #print(i+j)
+                if (A[i]+A[j]) == B:
 
+                    return 1
+        return 0
