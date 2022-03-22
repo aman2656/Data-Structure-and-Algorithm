260efd3af2155a4fa7baf321ff350a77af55da0a
Aman Gupta <42776181+aman2656@users.noreply.github.com>
2022-03-22 18:24:06 +0530

    Update Reverse the Array.py

--- Arrays/Introduction to Arrays/Reverse the Array.py
+++ Arrays/Introduction to Arrays/Reverse the Array.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    # @param A : tuple of integers
+    # @return a list of integers
+    def solve(self, A):
+        #A = [int(i) for i in A.split()]
+        #print('A', A)
+        A = [i for i in A]
+        #print('A', A)
+        l = 0
+        u = len(A)-1
+        #print(l,u)
+        while l<u:
+            #print('isnide while1')
+            A[u], A[l] = A[l], A[u]
+            #print('isnide while2')
+            #temp = 
+            l+=1
+            u-=1
+        #print(A)
+        return A
